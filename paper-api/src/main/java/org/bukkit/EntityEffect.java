a2bf91f2b56c6876d5b1c6988cb47b8e31f34955
ininjagod98 <181880040+ininjagod98@users.noreply.github.com>
2026-05-24 17:30:38 -0400

    Remove armorstandmonster

--- paper-api/src/main/java/org/bukkit/EntityEffect.java
+++ paper-api/src/main/java/org/bukkit/EntityEffect.java
@@ -6,7 +6,6 @@ import org.bukkit.entity.Allay;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.Armadillo;
 import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.ArmorStandMonster;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Cat;
 import org.bukkit.entity.Creaking;
@@ -269,11 +268,6 @@ public enum EntityEffect {
      * Armor stand is hit.
      */
     ARMOR_STAND_HIT(32, ArmorStand.class),
-    // 31 - unused
-    /**
-     * Armor stand is hit.
-     */
-    ARMOR_STAND_MONSTER_HIT(32, ArmorStandMonster.class),
     /**
      * Entity hurt by thorns attack.
      *
